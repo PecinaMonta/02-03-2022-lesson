ff2ec385a0ea0354786b1a8eabd4a3fae3e1d24f
Monta Pecina <montapecina@gmail.com>
2022-03-03 21:50:36 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createGlobalStyle } from 'styled-components';
+
+import AllPosts from "./components/AllPosts/AllPosts";
+import Form from "./components/Form/Form";
+import HomePage from "./components/HomePage/HomePage";
+import Nav from "./components/Nav/Nav";
+import Login from "./components/Login/Login";
+
+
+
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    font-family: sans-serif;
+    color: #112D4E;
+  }
+  /* a:visited  {
+    color: #112D4E;
+  } */
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyle/>
+    <BrowserRouter>
+      <Nav/>
+        <Routes>
+          <Route exact path="/" element={<HomePage/>} />
+          <Route exact path="/all-posts" element={<AllPosts/>} />
+          <Route exact path="/create" element={<Form/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="*" element={<div>404</div>} />
+        </Routes>        
+      </BrowserRouter>
+    </>
+      
   );
 }
 
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { FaPlusCircle } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
+import { useState } from "react";
+
+const NavBar = styled.header`
+    //usually 1rem = 16px
+    position: absolute;
+    height: 4rem;
+    padding: 1rem;
+    top: 0;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    width: 100%;
+    
+`
+
+const Logo = styled.a`
+    width: 2rem;
+    height: 2rem;
+    display: block;
+    background: #DBE2EF url('https://www.themealdb.com/images/category/beef.png') center/contain no-repeat;
+    justify-content: start;
+`
+
+const NavLink = styled(Link)`
+    min-width: 4rem;
+    padding: 0 0 0 1rem;
+    text-decoration: none;
+    &:visited {
+        color: #112D4E;
+    }
+    direction: start;
+`
+
+const ButtonLink = styled(NavLink)`
+    width: 2rem;
+    height: 2rem;
+    text-align: center;
+    direction: end;
+`
+
+const Nav = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+    return (
+        <NavBar>
+            <Logo href="/"/>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/all-posts">All posts</NavLink>
+            <ButtonLink to="/create"><FaPlusCircle/></ButtonLink>
+            <div to="/login" onClick={setIsOpen(!isOpen)}><FaUserAlt/></div>
+        </NavBar>
+    )
+}
+
+export default Nav
